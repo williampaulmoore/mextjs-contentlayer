2e8991a02fe38c54bd473b3b9786351bcd4197a0
Paul Moore <williampaulmoore@gmail.com>
2023-06-23 22:12:50 +0100

    feat: add blogs page

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from 'next/link';
+
+const blogs = () => {
+    return (
+        <>
+        <div  data-component-id="page/blogs/31c0cccd-1150-4ed5-8e3b-2054529a3897" >
+            Blog Entries
+        </div>
+        <Link href='/'>Home</Link>
+        </>
+    );
+}
+
+export default blogs;
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import React  from 'react';
+import Link from 'next/link';
+
 const page = () => {
-    return <div>Home</div>
+    return (
+        <>
+        <h1 data-component-id="page/home/e72b941f-6202-4d24-a933-b6e4d8d24786" >
+            Home
+        </h1>
+        <Link href='/blogs'>Blogs</Link>
+        </>
+    );
 }
 
 export default page;
